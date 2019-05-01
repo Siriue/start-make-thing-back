0b35abd3e5d79b49b45f9a2246c9d5b17851a85c
siriue <18340826486@163.com>
2019-05-01 11:52:27 +0800

    init

---
+++ src/main/java/com/dlmu/zhouxu/ZhouxuApplication.java
@@ -0,0 +1,13 @@
+package com.dlmu.zhouxu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZhouxuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZhouxuApplication.class, args);
+    }
+
+}
